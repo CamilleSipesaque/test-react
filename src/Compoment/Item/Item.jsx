04317e44ac59d1cc7b9040871b0b76c74da5eb08
antema102 <antema103@gmail.com>
2024-02-27 12:52:13 +0300

    mise a jour

--- src/Compoment/Item/Item.jsx
+++ src/Compoment/Item/Item.jsx
@@ -2,24 +2,24 @@ import './Item.scss';
 import React from "react";
 
 const Item = ({ data }) => {
-    const heightInCentimeters = data.height;
-    const meters = Math.floor(heightInCentimeters / 100);
-    const centimeters = heightInCentimeters % 100;
-  
-    return (
-      <div className="content-stars-wars-gird-item">
-        <div className="title">
-          <p>{data.name}</p>
-        </div>
-        <div className="text">
-          <p>Taille : {meters ? `${meters}m${centimeters}` : `${centimeters} cm`}</p>
-          <p>Poid : {data.mass === 'unknown' ? 'unknown' : `${data.mass} Kg`}</p>
-          <p>Cheveux : {data.hair_color}</p>
-          <p>Yeux : {data.eye_color}</p>
-          <p>Genre : {data.gender}</p>
-        </div>
-      </div>
-    );
-  };
+  const heightInCentimeters = data.height;
+  const meters = Math.floor(heightInCentimeters / 100);
+  const centimeters = heightInCentimeters % 100;
 
-  export default Item;
+  return (
+    <div className="content-stars-wars-gird-item">
+      <div className="title">
+        <p>{data.name}</p>
+      </div>
+      <div className="text">
+        <p>Taille : {meters ? `${meters}m${centimeters < 10 ? `0${centimeters}` : centimeters}` : `${centimeters} cm`}</p>
+        <p>Poid : {data.mass === 'unknown' ? 'unknown' : `${data.mass} Kg`}</p>
+        <p>Cheveux : {data.hair_color}</p>
+        <p>Yeux : {data.eye_color}</p>
+        <p>Genre : {data.gender}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
